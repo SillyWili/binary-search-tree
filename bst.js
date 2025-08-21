28ba21592b36817bd0e4cfd8c7ba7d607b7f5ec5
Wiliam Muller <174868022+SillyWili@users.noreply.github.com>
2025-08-21 02:42:08 +0200

    fix: sort function now sorts with integers

--- bst.js
+++ bst.js
@@ -6,7 +6,7 @@ class Node {
   }
 }
 
-class Tree {
+export default class Tree {
   constructor(array) {
     this.root = this.buildTree(array);
   }
@@ -14,9 +14,28 @@ class Tree {
   buildTree(array) {
     let set = new Set(array);
     let uniqueArr = [...set];
-    uniqueArr.sort();
+    uniqueArr.sort(function (a, b) {
+      return a - b;
+    });
 
     return this.#buildTreeRec(uniqueArr, 0, uniqueArr.length - 1);
+  }
+
+  prettyPrint(node = this.root, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
   }
 
   #buildTreeRec(arr, start, end) {
