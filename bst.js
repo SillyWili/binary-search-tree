e818bc4003b5b1c338864c27d65cf40d7ab0585a
Wiliam Muller <174868022+SillyWili@users.noreply.github.com>
2025-09-07 16:15:22 +0200

    add: new insert and remove methods

--- bst.js
+++ bst.js
@@ -21,6 +21,66 @@ export default class Tree {
     return this.#buildTreeRec(uniqueArr, 0, uniqueArr.length - 1);
   }
 
+  insert(root, value) {
+    if (root === null) {
+      return new Node(value);
+    }
+
+    if (root.data === value) {
+      return root;
+    }
+
+    if (value < root.data) {
+      root.left = this.insert(root.left, value);
+    } else if (value > root.data) {
+      root.right = this.insert(root.right, value);
+    }
+
+    return root;
+  }
+
+  remove(root, value) {
+    // first case where the node to delete is a leaf node with no children
+    if (root.left === null && root.right === null && root.data === value) {
+      return null;
+    }
+
+    // second case where the node to remove has a single child
+    if (root.data === value && root.right !== null && root.left === null) {
+      return root.right;
+    }
+
+    if (root.data === value && root.left !== null && root.right === null) {
+      return root.left;
+    }
+
+    if (root.data === value && root.right !== null && root.left !== null) {
+      let successor = this.#getSuccessor(root);
+
+      root.data = successor.data;
+
+      root.right = this.remove(root.right, successor.data);
+
+      return root;
+    }
+
+    if (value < root.data) {
+      root.left = this.remove(root.left, value);
+    } else if (value > root.data) {
+      root.right = this.remove(root.right, value);
+    }
+
+    return root;
+  }
+
+  #getSuccessor(node) {
+    node = node.right;
+    while (node !== null && node.left !== null) {
+      node = node.left;
+    }
+    return node;
+  }
+
   prettyPrint(node = this.root, prefix = "", isLeft = true) {
     if (node === null) {
       return;
