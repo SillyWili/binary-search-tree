c72ad650c8995a490b26bd23ffcdac3866141d5c
Wiliam Muller <174868022+SillyWili@users.noreply.github.com>
2025-09-07 16:16:41 +0200

    insert and remove tests passed

--- main.js
+++ main.js
@@ -2,5 +2,11 @@ import Tree from "./bst.js";
 
 const arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 const test = new Tree(arr);
+const root = test.root;
 
+test.prettyPrint();
+test.insert(root, 2);
+test.remove(root, 67);
+test.prettyPrint();
+test.remove(root, 3);
 test.prettyPrint();
